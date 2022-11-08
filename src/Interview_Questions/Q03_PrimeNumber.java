6b9a3d61f3c14b76447c5740b874b2518ead9622
QA Murat Tas <itmmt38@gmail.com>
2022-11-08 16:15:59 +0300

    interview

---
+++ src/Interview_Questions/Q03_PrimeNumber.java
@@ -0,0 +1,36 @@
+package Interview_Questions;
+
+import java.util.Scanner;
+
+public class Q03_PrimeNumber {
+    /*
+    Ask user enter a positive number and check if it is prime or not
+    Kullanıcıdan pozitif bir sayı girmesini isteyin ve asal olup olmadığını kontrol edin
+     */
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("bir sayı Giriniz");
+        int sayi =scan.nextInt();
+        int a =Math.abs(sayi);
+        boolean flag=true;
+
+        if (a>=2){
+            for (int i = 2; i <a ; i++) {
+                if (a%i==0){
+                    flag=false;
+                    break;
+                }
+            }
+        if (flag==true) {
+            System.out.println("sayi asaldir");
+
+        }else System.out.println("sayı asal değildir");
+
+
+        }else System.out.println("SAyı asal değildir");
+
+
+    }
+}
---
+++ src/Interview_Questions/Q07_IsDigit.java
@@ -0,0 +1,44 @@
+package Interview_Questions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q07_IsDigit {
+      /*
+     Create a method that accepts a String as parameter and finds the sum of digits in that String.
+     Example:
+     input : J4\/4 1$ 34$¥
+     output : 16
+
+     Hint:
+     Character.isDigit()
+     Integer.valueOf()
+    */
+
+    public static void main(String[] args) {
+      String str = "J4\\/4 1$ 34$¥" ;
+
+isDigit(str);
+
+
+
+
+    }
+
+    private static void isDigit(String str) {
+        int sum =0;
+        for (int i = 0; i <str.length() ; i++) {
+
+
+            if (Character.isDigit(str.charAt(i))) {
+
+
+                sum+=Integer.parseInt(String.valueOf(str.charAt(i)));// önce char at i Stringe çevirdi. daha sonra parse int ile integer a çevirdi.
+
+            }
+
+        }
+        System.out.println("sum = " + sum);
+
+    }
+}
